68a2d6423be0e217c010488ff332ab3b09345db5
Amit Sinha <amitfishy@gmail.com>
2018-04-03 12:02:04 +0530

    Fixed Float-Int Indexing Numpy version Errors

--- lib/roi_data_layer/minibatch.py
+++ lib/roi_data_layer/minibatch.py
@@ -173,6 +173,8 @@ def _get_bbox_regression_labels(bbox_target_data, num_classes):
         cls = clss[ind]
         start = 4 * cls
         end = start + 4
+        start=int(start)
+        end=int(end)
         bbox_targets[ind, start:end] = bbox_target_data[ind, 1:]
         bbox_inside_weights[ind, start:end] = cfg.TRAIN.BBOX_INSIDE_WEIGHTS
     return bbox_targets, bbox_inside_weights
